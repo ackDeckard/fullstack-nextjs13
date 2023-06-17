bbf87ac656c18ca92109f1cfa7fcd242cc5344ef
Pablo <ackdeckard@proton.me>
2023-06-17 12:33:27 -0300

    Base project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Full stack application using the following tools and concepts;
+
+      - Next.js 13.4.6
+      - Clerk for auth
+      - Tailwind
+      - Typescript
+      - Applying Artificial Intelligence
---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-black text-white">
+      <div className="mx-auto w-full max-w-[600px]">
+        <h1 className="mb-4 text-6xl">The best Journal app, period.</h1>
+        <p className="mb-4 text-2xl text-white/60">
+          This could be the best app for tracking your mood throught out your
+          life. All you have to do is be honest.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="rounded-lg bg-blue-600 px-4 py-2 text-xl ">
+              get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
